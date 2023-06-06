fd891aa14db4ef8ac6a9f1fc419a1a41106e106e
Your Name <you@example.com>
2023-06-06 20:45:26 +0000

    2-02 모델 중간까지 완료

--- projects/pybo/models.py
+++ projects/pybo/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
+class Question(models.Model):
+    subject = models.CharField(max_length=200) # 글자수 제한 Text
+    content = models.TextField() # 글자수 제한 불가
+    crate_date = models.DateTimeField()
+
+class Answer(models.Model):
+    # CASCADE: ForeignKey로 연결된 Question이 삭제되면 이 Answer을 삭제
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    content = models.TextField()
+    create_date = models.DateTimeField()
+
 # Create your models here.
